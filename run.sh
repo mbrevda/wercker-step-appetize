20e532872a4d74c56f1c740d2d17993f87754a87
Moshe Brevda <mbrevda@gmail.com>
2017-01-27 16:50:10 +0200

    echo, not cat

--- run.sh
+++ run.sh
@@ -29,5 +29,5 @@ then
 else
     info "app is avalible at https://appetize.io/app/$APPETIZE_KEY"
     #export WERCKER_APPETIZE_APPKEY=$APPETIZE_KEY
-    cat $APPETIZE_KEY > .appetize_appkey
+    echo $APPETIZE_KEY > .appetize_appkey
 fi

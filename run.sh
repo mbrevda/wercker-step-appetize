bc2852cf69dfa1e8086f6e15e994ecca4315340b
Moshe Brevda <mbrevda@gmail.com>
2017-01-27 15:30:33 +0200

    Fix field excaping

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ fi
 RES=$(curl https://$WERCKER_APPETIZE_TOKEN@api.appetize.io/v1/apps \
     -F "file=@$WERCKER_APPETIZE_PLATH" \
     -F "platform=$WERCKER_APPETIZE_PLATFORM" \
-    -f "note=revision $WERCKER_GIT_COMMIT, build url: $WERCKER_RUN_URL")
+    -f "note=\"revision $WERCKER_GIT_COMMIT, build url: $WERCKER_RUN_URL\"")
 
 APPETIZE_KEY=`echo $RES | grep -Po '(?<="publicKey":")(.*?)(?=",)'`
 if [[ -z $APPETIZE_KEY ]]

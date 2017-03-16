71e9b1043ea9f858c8a6a250b6f84470306a2b5c
Moshe Brevda <mbrevda@gmail.com>
2017-03-16 14:37:37 +0200

    Add branch to notes

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ fi
 RES=$(curl https://$WERCKER_APPETIZE_TOKEN@api.appetize.io/v1/apps \
     -F "file=@$WERCKER_APPETIZE_PATH" \
     -F "platform=$WERCKER_APPETIZE_PLATFORM" \
-    -F "note=\"revision $WERCKER_GIT_COMMIT, build url: $WERCKER_RUN_URL\"")
+    -F "note=\"branch: $WERCKER_GIT_BRANCH; revision $WERCKER_GIT_COMMIT; build url: $WERCKER_RUN_URL\"")
 
 APPETIZE_KEY=`echo $RES | grep -Po '(?<="publicKey":")(.*?)(?=",)'`
 if [[ -z $APPETIZE_KEY ]]
